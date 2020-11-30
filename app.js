05c3a1412470fde37ceb33c0474eb9f14314335a
Joe <josephfmck@gmail.com>
2020-11-30 15:23:18 -0600

    add event listeners for backBtn that l.clearEditState()

--- app.js
+++ app.js
@@ -262,6 +262,9 @@ const AppCtrl = (function(ItemCtrl, UICtrl) {
 
         //  Update item event
         document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
+        //  Back button event
+        document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState());
     }
 
     //  Add item submit
